c2e98f4feb0adaf200a7cdceda87b57866d3b9ed
keyingchang <keyingchang@ccclubs.com>
2018-09-20 19:35:10 +0800

    （1）新的文件；

---
+++ app/src/main/java/com/robot/toolsman/bean/HomeBean1.kt
@@ -0,0 +1,29 @@
+package com.robot.toolsman.bean
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class HomeBean(
+        val text: String?,
+        val imgList: List<HomeInnerBean>?
+) : Parcelable {
+    constructor(source: Parcel) : this(
+            source.readString(),
+            source.createTypedArrayList(HomeInnerBean.CREATOR)
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(text)
+        writeTypedList(imgList)
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<HomeBean> = object : Parcelable.Creator<HomeBean> {
+            override fun createFromParcel(source: Parcel): HomeBean = HomeBean(source)
+            override fun newArray(size: Int): Array<HomeBean?> = arrayOfNulls(size)
+        }
+    }
+}
